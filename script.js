9d276168fece917213fe4a72631e4c31d7d600ce
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-01 23:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const name=document.getElementById("name");
 const year=document.getElementById("year");
 const submit=document.getElementById("button");
 
-submit.addEventListener("click", ()=>{
+submit.addEventListener("click", (e)=>{
 	e.preventDefault();
 
 	if(name.value==""){
-		text="https://localhost:8080/"+"?"+"year"+year.value;
+		text="https://localhost:8080/"+"?"+"year="+year.value;
 	}else if(year.value==""){
-		text="https://localhost:8080/"+"?"+"name"+name.value;
+		text="https://localhost:8080/"+"?"+"name="+name.value;
 	}else if(name.value!="" && name.value!==""){
-		text="https://localhost:8080/"+"?"+"name"+name.value+;
+		text="https://localhost:8080/"+"?"+"name="+name.value+"&"+"year="+year.value;
+	}else{
+		text="https://localhost:8080/";
 	}
+	heading.innerText=text;
 })
