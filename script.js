065c03d073647264304a835f7a1042486371cb36
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-01 23:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const heading=document.getElementById("url");
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const submit=document.getElementById("button");
+
+submit.addEventListener("click", ()=>{
+	e.preventDefault();
+
+	if(name.value==""){
+		heading.innerText="https://localhost:8080/"+year
+	}
+})
